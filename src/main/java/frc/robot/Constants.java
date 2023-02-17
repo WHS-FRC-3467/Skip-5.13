86d6ba78e0c4224541741ed387a2788c3b901bba
Driver Station <apanteli23@team3467.org>
2023-02-16 21:17:43 -0500

    Driving at the end of 2/16

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -73,13 +73,13 @@ public final class Constants {
 
     //Gold Bot
     //Mod 0
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = 290.3;
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = 289.2;
     //Mod 1
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = 74.1;
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = 74.6;
     //Mod 2
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = 290.4; 
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = 291.4; 
     //Mod 3
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = 206.7;
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = 206.4;
 
     public static final boolean INVERT_GYRO = false; // Always ensure Gyro is CCW+ CW-
 
@@ -195,7 +195,7 @@ public final class Constants {
             new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
 
-    public static final Gains GAINS_ANGLE_SNAP = new Gains(0.025, 0.0, 0.0, 0.0, 50);
+    public static final Gains GAINS_ANGLE_SNAP = new Gains(0.021, 0.0, 0.0, 0.0, 50);
     
     public static final Gains GAINS_BALANCE = new Gains(0.025, 0.0, 0.0, 0.0, 50);
 
@@ -205,11 +205,11 @@ public final class Constants {
 
   public static final class ArmConstants{
     //Arm offsets
-    public static final double VERTICAL_ANGLE_UPPER = 188.4;
+    public static final double VERTICAL_ANGLE_UPPER = 20.8;
     public static final double VERTICAL_ANGLE_LOWER = 180.0;
 
     public static final double LOWER_ANGLE_OFFSET = 180 - VERTICAL_ANGLE_LOWER;
-    public static final double UPPER_ANGLE_OFFSET = 180 - VERTICAL_ANGLE_UPPER;
+    public static final double UPPER_ANGLE_OFFSET = 12.4 - VERTICAL_ANGLE_UPPER;
 
     //Gains
     public static final  Gains GAINS_UPPER_JOINT = new Gains(0.02, 0.0, 0.00, 0.0, 50);
@@ -272,15 +272,15 @@ public final class Constants {
     public static final Setpoint MANUAL_MODE = new Setpoint(180, 16, false, ClawState.IN, 180, 16, false, ClawState.OUT, ArmState.OTHER);
 
     public static final Setpoint FLOOR = new Setpoint(245, 61, true, ClawState.IN, 245, 62, true, ClawState.OUT, ArmState.FLOOR);
-    public static final Setpoint MID_NODE = new Setpoint(175.5, 80, false, ClawState.IN, 192, 71, false, ClawState.OUT, ArmState.MID_NODE);
-    public static final Setpoint MID_NODE_PLACED = new Setpoint(183.5, 72, false, ClawState.IN, 192, 69, false, ClawState.OUT, ArmState.MID_NODE_PLACED);
-    public static final Setpoint MID_NODE_PLACED_AND_OPEN = new Setpoint(183.5, 72, false, ClawState.IN, 192, 69, false, ClawState.OUT, ArmState.MID_NODE_PLACED);
-    public static final Setpoint TOP_NODE = new Setpoint(212, 143, false, ClawState.IN, 213, 118, false, ClawState.OUT, ArmState.TOP_NODE);
-    public static final Setpoint TOP_NODE_PLACED = new Setpoint(218, 140, false, ClawState.IN, 213, 115, false, ClawState.OUT, ArmState.TOP_NODE_PLACED);
+    public static final Setpoint MID_NODE = new Setpoint(179, 86, false, ClawState.IN, 192, 71, false, ClawState.OUT, ArmState.MID_NODE);
+    public static final Setpoint MID_NODE_PLACED = new Setpoint(184.5, 77, false, ClawState.IN, 192, 69, false, ClawState.OUT, ArmState.MID_NODE_PLACED);
+    public static final Setpoint MID_NODE_PLACED_AND_OPEN = new Setpoint(183.5, 76, false, ClawState.IN, 192, 69, false, ClawState.OUT, ArmState.MID_NODE_PLACED);
+    public static final Setpoint TOP_NODE = new Setpoint(214, 141, false, ClawState.IN, 213, 118, false, ClawState.OUT, ArmState.TOP_NODE);
+    public static final Setpoint TOP_NODE_PLACED = new Setpoint(218, 139, false, ClawState.IN, 213, 115, false, ClawState.OUT, ArmState.TOP_NODE_PLACED);
     public static final Setpoint TOP_NODE_PLACED_AND_OPEN = new Setpoint(218, 140, false, ClawState.IN, 213, 115, false, ClawState.OUT, ArmState.TOP_NODE_PLACED);
     public static final Setpoint SUBSTATION = new Setpoint(150, 53, false, ClawState.IN, 150, 53, false, ClawState.OUT, ArmState.SUBSTATION);
 
-    public static final double INTERMEDIATE_LOWER_POSITION = 150;
+    public static final double INTERMEDIATE_LOWER_POSITION = 154;
   }
   public static final class ClawConstants{
     public static final double CLAW_BASE_CURRENT = 2.0;
