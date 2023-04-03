b42487a784d4669444465740354962baec9073e0
Driver Station <apanteli23@team3467.org>
2023-04-03 18:46:33 -0400

    Added framework for spark max claw

--- src/main/java/frc/robot/subsystems/claw/ClawSubsytem.java
+++ src/main/java/frc/robot/subsystems/claw/ClawSubsytem.java
@@ -9,6 +9,10 @@ import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -17,12 +21,18 @@ import frc.robot.Constants.CanConstants;
 public class ClawSubsytem extends SubsystemBase {
   /** Creates a new ClawSubsytem. */
   private TalonSRX m_clawMotor = new TalonSRX(CanConstants.CLAW_MOTOR);
+  //private CANSparkMax m_clawMotor = new CANSparkMax(16, MotorType.kBrushless);
   public ClawSubsytem() {
     m_clawMotor.configFactoryDefault();
     m_clawMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 10, 15, 0.5));
     m_clawMotor.configVoltageCompSaturation(12.0);
     m_clawMotor.setNeutralMode(NeutralMode.Coast);
     m_clawMotor.configOpenloopRamp(0.2);
+    // m_clawMotor.restoreFactoryDefaults();
+    // m_clawMotor.setOpenLoopRampRate(0.2);
+    // m_clawMotor.enableVoltageCompensation(12.0);
+    // m_clawMotor.setSmartCurrentLimit(10, 10);
+    // m_clawMotor.setIdleMode(IdleMode.kBrake);
   }
 
   
@@ -35,9 +45,11 @@ public class ClawSubsytem extends SubsystemBase {
   }
   public void driveClaw(double speed){
     m_clawMotor.set(TalonSRXControlMode.PercentOutput, speed);
+    //m_clawMotor.set(speed);
   }
   public double getClawCurrent(){
     return m_clawMotor.getSupplyCurrent();
+    //return m_clawMotor.getOutputCurrent();
   }
   
 }
