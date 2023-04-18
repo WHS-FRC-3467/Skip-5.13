8a6141ed12e0215bf985203078ae21220960b2b5
Laptop 5 <apanteli23@team3467.org>
2023-04-18 15:32:39 -0400

    Added some print stuff and removed duplicate event markers

--- src/main/java/frc/robot/auto/TwoPiece.java
+++ src/main/java/frc/robot/auto/TwoPiece.java
@@ -13,12 +13,12 @@ import com.pathplanner.lib.commands.FollowPathWithEvents;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.ArmSetpoints;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.GoToPositionWithIntermediate;
-import frc.robot.subsystems.arm.RetractFromFloor;
 import frc.robot.subsystems.arm.RetractToStowed;
 import frc.robot.subsystems.arm.ScoreAndRetract;
 import frc.robot.subsystems.arm.ScoreOnGrid;
@@ -36,16 +36,23 @@ public class TwoPiece extends SequentialCommandGroup {
     PathPlannerTrajectory twoPiecePath = PathPlanner.loadPath("TwoPiece", new PathConstraints(2.0, 4.0));
     HashMap<String, Command> eventMap = new HashMap<>();
  
-    eventMap.put("Retract", new SequentialCommandGroup(
-                              new WaitCommand(0.25),
+    eventMap.put("Retract", new ParallelCommandGroup(
+                              Commands.print("Retract"),
                               new RetractToStowed(arm)));
-    eventMap.put("Retract floor", new SequentialCommandGroup(
-                              new WaitCommand(0.25),
-                              new RetractFromFloor(arm)));
-    eventMap.put("Floor position", Commands.runOnce(()-> arm.updateAllSetpoints(ArmSetpoints.FLOOR)));
-    eventMap.put("Run intake", Commands.run(()->claw.driveClaw(1.0)));
-    eventMap.put("Claw quarter", Commands.run(()->claw.driveClaw(0.25)));
-    eventMap.put("go to scoring position", new GoToPositionWithIntermediate(arm, ArmSetpoints.TOP_NODE));
+                              
+    eventMap.put("Floor position", new ParallelCommandGroup(
+                                    Commands.runOnce(()-> arm.updateAllSetpoints(ArmSetpoints.FLOOR)),
+                                    Commands.run(()->claw.driveClaw(1.0)),
+                                    Commands.print("Floor Position")));
+                                              
+    eventMap.put("Retract floor", new ParallelCommandGroup(
+                              Commands.print("Retract Floor"),
+                              new RetractToStowed(arm),
+                              Commands.run(()-> claw.driveClaw(0.25))));
+
+    eventMap.put("go to scoring position", new ParallelCommandGroup(
+                                            Commands.print("Go to scoring position"),
+                                            new GoToPositionWithIntermediate(arm, ArmSetpoints.TOP_NODE)));
     
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
@@ -55,9 +62,10 @@ public class TwoPiece extends SequentialCommandGroup {
       new GoToPositionWithIntermediate(arm, ArmSetpoints.TOP_NODE),
       new ScoreOnGrid(arm, claw),      
       Commands.runOnce(() -> GamePiece.setGamePiece(GamePieceType.Cube)),
+      Commands.print(eventMap.toString()),
       new FollowPathWithEvents(drive.followTrajectoryCommand(twoPiecePath, true), 
                               twoPiecePath.getMarkers(), 
-                              eventMap),      
+                              eventMap),     
       new ScoreAndRetract(arm, claw)
     );
   }
